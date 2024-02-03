dea09c2caca19ab63a6a0a00de24a86f539e0eb8
kanyi <jkanyi757@gmail.com>
2024-02-03 23:38:46 +0300

    initial commit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
   respond_to :json
+  before_action :authenticate_user!, only: [:destroy]
 
   def create
     super do |user|
@@ -21,18 +22,34 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    jwt_payload = JWT.decode(request.headers['Authorization'].split[1],
-                             Rails.application.credentials.fetch(:secret_key_base)).first
-    current_user = User.find(jwt_payload['sub'])
-    if current_user
-      render json: {
-        status: 200,
-        message: 'Signed out successfully.'
-      }, status: :ok
+    auth_header = request.headers['Authorization']
+    if auth_header
+      begin
+        token = auth_header.split[1]
+        jwt_payload = JWT.decode(token, Rails.application.credentials.fetch(:secret_key_base)).first
+        current_user = User.find_by(id: jwt_payload['sub'])
+        if current_user
+          User.revoke_jwt(nil, current_user)
+          render json: {
+            status: 200,
+            message: 'Signed out successfully.'
+          }, status: :ok
+        else
+          render json: {
+            status: 401,
+            message: "Couldn't find an active session."
+          }, status: :unauthorized
+        end
+      rescue JWT::DecodeError => e
+        render json: {
+          status: 401,
+          message: "Invalid token: #{e.message}"
+        }, status: :unauthorized
+      end
     else
       render json: {
         status: 401,
-        message: "Couldn't find an active session."
+        message: "Missing token."
       }, status: :unauthorized
     end
   end
